b45b9b02289c8502cc3549d5690ab6e823ea89d6
Pedro Duarte <contact@peduarte.com>
2015-08-07 18:05:39 +0100

    Update share icons to actually share something

--- src/components/share/share.js
+++ src/components/share/share.js
@@ -8,8 +8,21 @@ import FacebookIcon from '../assets/facebook.js';
 export default React.createClass({
   render() {
 
+    var self = this;
+
+    var shareUrl = function() {
+      switch(self.props.network) {
+        case 'facebook':
+          return 'https://www.facebook.com/sharer/sharer.php?u=http://pedroduarte.me/london-housing/';
+          break;
+         case 'twitter':
+          return  'https://twitter.com/intent/tweet?url=http://pedroduarte.me/london-housing/&text=London%20Housing%20Headlines&hashtags=london,housing';
+          break;
+      }
+    }();
+
     return (
-      <a className="Button Share">
+      <a className="Button Share" href={shareUrl} target="_blank">
         {this.props.children}
       </a>
     )
